9213dfcfa4c095c8b0057a03ed02a779d955a316
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2022-12-23 21:26:33 +0700

    Update and rename savePicture.py to imgTools.py

--- imgTools.py
+++ imgTools.py
@@ -1,7 +1,9 @@
 import os
 import cv2
 import os
-from maskdetect_config import *
+import base64
+import requests
+from config import *
 
 def saveFace(faceframe, savedir):
     fileindir = len([
@@ -18,3 +20,12 @@ def saveFull(frame, savedir):
     ])
     filename = savedir + fileprefix + str(fileindir) + filetype
     cv2.imwrite(filename, frame)
+
+def sendImage(frame):
+    _, image_jpg = cv2.imencode('.jpg', frame)
+    image_bytes = image_jpg.tostring()
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    response = requests.post(serverURL, json={'image': image_base64})
+    res = response.json()
+    filename = res['filename']
+    return filename
